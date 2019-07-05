8693fa20ffeffeec923df96bcc2adb948f804f72
ascutariu <andreiscutariu92@gmail.com>
2019-07-05 18:53:49 +0300

    updates - add entities

---
+++ UserPosts/UserPosts.Domain/User.cs
@@ -0,0 +1,13 @@
+﻿namespace UserPosts.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+    }
+}
